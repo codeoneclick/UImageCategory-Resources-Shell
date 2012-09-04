85d91a4bddf5cd61002b232ba2d82899e7d915dd
Sergey Sergeev <sergey.sergeev@yes.local>
2012-09-04 09:31:24 +0300

    added template for shell script

--- UImageCategory(Resources)Shell/AppDelegate.m
+++ UImageCategory(Resources)Shell/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "UImage+Resources.h"
 
 @implementation AppDelegate
 
@@ -16,6 +17,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    UIImage *image = [UIImage ico_contacts_png_image];
     return YES;
 }
 
