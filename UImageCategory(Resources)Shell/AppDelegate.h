f61463f382ad829dd4190b779a237fa5c5492110
Sergey Sergeev <sergey.sergeev@yes.local>
2012-09-04 09:04:45 +0300

    Initial Commit

---
+++ UImageCategory(Resources)Shell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UImageCategory(Resources)Shell
+//
+//  Created by Sergey Sergeev on 9/4/12.
+//  Copyright (c) 2012 Sergey Sergeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UImageCategory(Resources)Shell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UImageCategory(Resources)Shell
+//
+//  Created by Sergey Sergeev on 9/4/12.
+//  Copyright (c) 2012 Sergey Sergeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
